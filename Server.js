382b12a03a9bae8c5ae163f540c206c65e5b40b3
vickel leung <>
2020-09-13 20:47:06 -0400

    20: Modified design of restAPI

--- Server.js
+++ Server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+let routes = require("./routes/routes.js");
+
+app.get("/signin", (req, res) => {
+  routes.signIn(res);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,46 @@
-let User = require("./model/user");
+// let User = require("model/user");
+const express = require("express");
+const app = express();
 
-// Signin API
-app.get("/sigin", (req, res) => {
-  res.send("Hello World!");
-});
+module.exports = {
+  // Signin API
+  signIn: (res) => {
+    // app.get("/signin", (req, res) => {
+    //   res.send("Hello World!");
+    // });
+    res.send("hello");
+  },
 
-// Registration API
-app.get("/register", (req, res) => {
-  res.send("Hello World!");
-});
+  // Registration API
+  register: () => {
+    app.get("/register", (req, res) => {
+      res.send("Hello World!");
+    });
+  },
 
-// Services API
-app.get("/services", (req, res) => {
-  res.send("Hello World!");
-});
+  // Services API
+  services: () => {
+    app.get("/services", (req, res) => {
+      res.send("Hello World!");
+    });
+  },
 
-app.get("/services/id", (req, res) => {
-  res.send("Hello World!");
-});
+  getServiceByID: () => {
+    app.get("/services/id", (req, res) => {
+      res.send("Hello World!");
+    });
+  },
 
-// Shop API
-app.get("/shop", (req, res) => {
-  res.send("Hello World!");
-});
+  // Shop API
+  shop: () => {
+    app.get("/shop", (req, res) => {
+      res.send("Hello World!");
+    });
+  },
 
-app.get("/shop/id", (req, res) => {
-  res.send("Hello World!");
-});
+  getShopById: () => {
+    app.get("/shop/id", (req, res) => {
+      res.send("Hello World!");
+    });
+  },
+};
