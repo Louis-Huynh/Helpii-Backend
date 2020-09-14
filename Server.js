925f622208460b5bc5ccb224c41c0a2fca2d06f2
sourpaper <louisglhuynh@gmail.com>
2020-09-13 21:30:17 -0400

    20:refactored routes

--- Server.js
+++ Server.js
@@ -5,7 +5,27 @@ const port = process.env.PORT || 3000;
 let routes = require("./routes/routes.js");
 
 app.get("/signin", (req, res) => {
-  routes.signIn(res);
+  routes.signIn(req, res);
+});
+
+app.get("/services", (req, res) => {
+  routes.services(req, res);
+});
+
+app.get("/register", (req, res) => {
+  routes.register(req, res);
+});
+
+app.get("/services/id", (req, res) => {
+  routes.getServiceByID(req, res);
+});
+
+app.get("/shop", (req, res) => {
+  routes.shop(req, res);
+});
+
+app.get("/shop/id", (req, res) => {
+  routes.getShopById(req, res);
 });
 
 app.listen(port, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -4,43 +4,29 @@ const app = express();
 
 module.exports = {
   // Signin API
-  signIn: (res) => {
-    // app.get("/signin", (req, res) => {
-    //   res.send("Hello World!");
-    // });
+  signIn: (req, res) => {
     res.send("hello");
   },
 
   // Registration API
-  register: () => {
-    app.get("/register", (req, res) => {
-      res.send("Hello World!");
-    });
+  register: (req, res) => {
+    res.send("Hello World!");
   },
-
   // Services API
-  services: () => {
-    app.get("/services", (req, res) => {
-      res.send("Hello World!");
-    });
+  services: (req, res) => {
+    res.send("Hello World!");
   },
 
-  getServiceByID: () => {
-    app.get("/services/id", (req, res) => {
-      res.send("Hello World!");
-    });
+  getServiceByID: (req, res) => {
+    res.send("Hello World!");
   },
 
   // Shop API
-  shop: () => {
-    app.get("/shop", (req, res) => {
-      res.send("Hello World!");
-    });
+  shop: (req, res) => {
+    res.send("Hello World!");
   },
 
-  getShopById: () => {
-    app.get("/shop/id", (req, res) => {
-      res.send("Hello World!");
-    });
+  getShopById: (req, res) => {
+    res.send("Hello World!");
   },
 };
