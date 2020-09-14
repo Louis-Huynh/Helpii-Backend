7faa26832e90f18f177619c1e209d29f74f1714e
sourpaper <louisglhuynh@gmail.com>
2020-09-13 22:01:13 -0400

    21: added create and register functions, and nodemon

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,33 @@
-// let User = require("model/user");
-const express = require("express");
-const app = express();
+const bcrypt = require("bcrypt");
 
 module.exports = {
   // Signin API
   signIn: (req, res) => {
+    let hash = "$2b$08$QtEab42ndLWinNQhxh8AGei/q7SOWpeO.zVCC6OL9s3166t/xxHDS";
+    let myPlaintextPassword = "helloae";
+
+    bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+      // result == true
+      if (result) {
+        console.log("it's good");
+      } else {
+        console.log("it's bad");
+      }
+    });
+
     res.send("signin");
   },
 
   // Registration API
   register: (req, res) => {
+    const saltRounds = Math.floor(Math.random() * 10);
+    const myPlaintextPassword = "hello";
+
+    bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
+      console.log(hash);
+    });
+
     res.send("register");
   },
   // Services API
